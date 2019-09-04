f0ea24408037efb7e06a45a7d46046d69687502e
= <sinqia@3P5_24.classroom.net>
2019-09-04 15:57:02 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//document.getElementById("tabela").style.display = "none";
+// console.log("to aqui");
+// document.getElementById("button").addEventListener('click', function(){
+//     document.getElementById("button").style.display = "none";
+// });
+function tabela(){
+    document.getElementById("button").style.display = "block";
+}
